3aeea633718ecad3a236074467ae20f3048701a8
murtaza-khan <murtazakhancs@gmail.com>
2023-01-14 18:15:32 +0500

    update url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL="https://user-posts-six.vercel.app//api"
+export const BASE_API_URL="https://user-posts-six.vercel.app/api"
 // export const BASE_API_URL="http://localhost:5005/api";
 export const config = {
     headers: {
